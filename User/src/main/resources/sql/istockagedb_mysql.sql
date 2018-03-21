3a13fa362a2792b5ab3b47f145c904214cf40b7c
chengjhan <chengjhan@gmail.com>
2018-03-22 00:06:23 +0800

    sql file

--- User/src/main/resources/sql/istockagedb_mysql.sql
+++ User/src/main/resources/sql/istockagedb_mysql.sql
@@ -7,22 +7,39 @@ create database if not exists istockagedb;
 use istockagedb;
 
 -- CREATE
--- Admin
-create table category (
-    ca_id                   int auto_increment not null,
-    ca_name                 nvarchar(20) not null,
-    primary key (ca_id)
+-- System
+create table path_category (
+    pc_id                   int auto_increment not null,
+    pc_name                 varchar(10) not null,
+    pc_extension            varchar(10) not null,
+    primary key (pc_id)
+);
+
+create table user_path (
+    up_id                   int auto_increment not null,
+    up_pc_id                int not null,
+    up_path                 varchar(50) not null,
+    up_name                 nvarchar(20) not null,
+    primary key (up_id),
+    foreign key (up_pc_id) references path_category (pc_id)
+);
+
+create table code_category (
+    cc_id                   int auto_increment not null,
+    cc_name                 nvarchar(20) not null,
+    primary key (cc_id)
 );
 
 create table code (
     co_id                   int auto_increment not null,
-    co_ca_id                int not null,
+    co_cc_id                int not null,
     co_name                 nvarchar(20) not null,
     co_no                   tinyint,
     primary key (co_id),
-    foreign key (co_ca_id) references category (ca_id)
+    foreign key (co_cc_id) references code_category (cc_id)
 );
 
+-- Admin
 create table broker_head (
     bh_id                   int auto_increment not null,
     bh_name                 nvarchar(20) not null,
@@ -54,6 +71,17 @@ create table member (
     me_update_info_time     datetime not null,
     me_update_pwd_time      datetime not null,
     primary key (me_id)
+);
+
+create table member_log (
+    ml_id                   int auto_increment not null,
+    ml_insert_time          timestamp default current_timestamp not null,
+    ml_me_id                int not null,
+    ml_up_id                int not null,
+    ml_ip                   varchar(20),
+    primary key (ml_id),
+    foreign key (ml_me_id) references member (me_id),
+    foreign key (ml_up_id) references user_path (up_id)
 );
 
 create table account (

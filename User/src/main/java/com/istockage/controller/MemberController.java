e4579d99f5250202a4bd4f6515729f4adc5b5ad6
chengjhan <chengjhan@gmail.com>
2018-03-29 13:35:39 +0800

    log4j

--- User/src/main/java/com/istockage/controller/MemberController.java
+++ User/src/main/java/com/istockage/controller/MemberController.java
@@ -3,7 +3,7 @@
  * File: MemberController.java
  * Author: 詹晟
  * Created: 2018/3/26
- * Modified: 2018/3/28
+ * Modified: 2018/3/29
  * Version: 1.0
  * Since: JDK 1.8
  */
@@ -11,6 +11,7 @@ package com.istockage.controller;
 
 import static com.istockage.common.constant.ModelAttributeConstant.USER;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,6 +32,10 @@ import com.istockage.model.service.MemberService;
 @Controller
 @SessionAttributes(USER)
 public class MemberController implements ControllerConstant {
+
+	private static final Logger logger = Logger.getLogger(MemberController.class);
+
+	private String className = this.getClass().getSimpleName();
 
 	/**
 	 * 注入 MemberService
@@ -63,6 +68,8 @@ public class MemberController implements ControllerConstant {
 	@RequestMapping(value = "/secure/sign-in.do", method = RequestMethod.POST)
 	public String signInAction(@RequestParam String me_email, @RequestParam String me_password, Model model) {
 
+		String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+
 		MemberEntity user = memberService.signIn(me_email, me_password);
 
 		if (user == null) {
@@ -71,11 +78,17 @@ public class MemberController implements ControllerConstant {
 			model.addAttribute(MEMBER_EMAIL, me_email);
 			model.addAttribute(MEMBER_PASSWORD, me_password);
 
+			logger.error("(" + className + "." + methodName + ") 登入失敗: 帳號或密碼錯誤");
+
 			return MEMBER_SIGN_IN_VIEW;
+
 		} else {
 
 			// 放入 Session
 			model.addAttribute(USER, user);
+
+			logger.info(
+					"(" + className + "." + methodName + ") 登入成功，使用者: " + user.getMe_email() + "，導向首頁: " + INDEX_VIEW);
 
 			return REDIRECT + INDEX_VIEW;
 		}
@@ -107,7 +120,11 @@ public class MemberController implements ControllerConstant {
 	@RequestMapping(value = "/member/sign-up.do", method = RequestMethod.POST)
 	public String signUpAction(MemberEntity memberEntity) {
 
+		String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+
 		memberService.signUp(memberEntity);
+
+		logger.info("(" + className + "." + methodName + ") 註冊成功");
 
 		return REDIRECT + INDEX_VIEW;
 	}

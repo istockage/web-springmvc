79a673a23d6d7d2d5fbb02a61957261b8dd56bce
chengjhan <chengjhan@gmail.com>
2018-04-08 00:43:59 +0800

    reset password interceptor

---
+++ User/src/main/java/com/istockage/interceptor/ResetPasswordInterceptor.java
@@ -0,0 +1,72 @@
+/*
+ * iStockage
+ * File: ResetPasswordInterceptor.java
+ * Author: 詹晟
+ * Created: 2018/4/8
+ * Modified: 2018/4/8
+ * Version: 1.0
+ * Since: JDK 1.8
+ */
+package com.istockage.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.istockage.common.constant.ControllerConstant;
+import com.istockage.common.util.StringUtil;
+
+/**
+ * reset password interceptor
+ * 
+ * @author 詹晟
+ */
+public class ResetPasswordInterceptor implements HandlerInterceptor, ControllerConstant {
+
+	private static final Logger logger = Logger.getLogger(ResetPasswordInterceptor.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HandlerMethod handlerMethod = (HandlerMethod) handler;
+		String handlerClassName = handlerMethod.getBeanType().getSimpleName();
+		String handlerMethodName = handlerMethod.getMethod().getName();
+
+		logger.info("(" + handlerClassName + "." + handlerMethodName + ") start");
+
+		String requestPath = StringUtil.getRequestPath(request.getServletPath(), request.getQueryString()); // 請求 path
+
+		if ((String) request.getSession().getAttribute(SESSION_MEMBER_EMAIL) == null) {
+
+			logger.info("(" + handlerClassName + "." + handlerMethodName + ") end, 攔截: " + requestPath);
+
+			response.sendRedirect(request.getContextPath() + SLASH + MEMBER_SIGN_IN_VIEW);
+
+			return false;
+
+		} else {
+
+			logger.info("(" + handlerClassName + "." + handlerMethodName + ") end, 放行: " + requestPath);
+
+			return true;
+		}
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+}

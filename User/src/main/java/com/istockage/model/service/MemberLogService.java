7c52c343051ea506335a4ed131fe7fc08039c327
chengjhan <chengjhan@gmail.com>
2018-04-12 13:24:45 +0800

    extends

--- User/src/main/java/com/istockage/model/service/MemberLogService.java
+++ User/src/main/java/com/istockage/model/service/MemberLogService.java
@@ -3,12 +3,13 @@
  * File: MemberLogService.java
  * Author: 詹晟
  * Created: 2018/3/28
- * Modified: 2018/3/28
+ * Modified: 2018/4/12
  * Version: 1.0
  * Since: JDK 1.8
  */
 package com.istockage.model.service;
 
+import com.istockage.common.constant.CodeConstant;
 import com.istockage.model.entity.MemberLogEntity;
 
 /**
@@ -16,7 +17,7 @@ import com.istockage.model.entity.MemberLogEntity;
  * 
  * @author 詹晟
  */
-public interface MemberLogService {
+public interface MemberLogService extends CodeConstant {
 
 	/**
 	 * @see com.istockage.model.service.impl.MemberLogServiceImpl#insert(MemberLogEntity)
--- User/src/main/java/com/istockage/model/service/UserPathService.java
+++ User/src/main/java/com/istockage/model/service/UserPathService.java
@@ -3,12 +3,13 @@
  * File: UserPathService.java
  * Author: 詹晟
  * Created: 2018/3/28
- * Modified: 2018/3/28
+ * Modified: 2018/4/12
  * Version: 1.0
  * Since: JDK 1.8
  */
 package com.istockage.model.service;
 
+import com.istockage.common.constant.CodeConstant;
 import com.istockage.model.entity.UserPathEntity;
 
 /**
@@ -16,7 +17,7 @@ import com.istockage.model.entity.UserPathEntity;
  * 
  * @author 詹晟
  */
-public interface UserPathService {
+public interface UserPathService extends CodeConstant {
 
 	/**
 	 * @see com.istockage.model.service.impl.UserPathServiceImpl#selectByUp_path(String,

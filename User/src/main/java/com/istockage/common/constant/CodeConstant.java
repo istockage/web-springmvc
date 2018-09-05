b01370ddc2b9a4c0e4f413de8161fcbd106fde2e
chengjhan <chengjhan@gmail.com>
2018-09-05 17:51:10 +0800

    code constant

--- User/src/main/java/com/istockage/common/constant/CodeConstant.java
+++ User/src/main/java/com/istockage/common/constant/CodeConstant.java
@@ -7,4 +7,15 @@ public interface CodeConstant {
 
 	public static final byte MEMBER_ACTIVITY_OPEN = 1;
 
+	// stock
+	public static final String STOCK_TYPE_CODE_CATEGORY = "買賣類別";
+
+	public static final byte SPOT_SHARE_CODE = 1;
+
+	public static final byte MARGIN_PURCHASE_CODE = 2;
+
+	public static final byte SHORT_SALE_CODE = 3;
+
+	public static final byte DRAWING_LOTS_CODE = 4;
+
 }

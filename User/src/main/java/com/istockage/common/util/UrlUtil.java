b9dfdb6b17a656948dc8d018cdec3d2ba4e1fe75
chengjhan <chengjhan@gmail.com>
2018-09-17 17:23:42 +0800

    UrlUtil

--- User/src/main/java/com/istockage/common/util/UrlUtil.java
+++ User/src/main/java/com/istockage/common/util/UrlUtil.java
@@ -7,18 +7,6 @@ public class UrlUtil {
 		return servletPath.substring(1, servletPath.length());
 	}
 
-	public static String getExtension(String servletPath) {
-
-		String lastPath = servletPath.split("/")[servletPath.split("/").length - 1];
-		int indexOfDot = lastPath.lastIndexOf(".");
-
-		if (indexOfDot != -1) {
-			return lastPath.substring(indexOfDot + 1, lastPath.length());
-		} else {
-			return "";
-		}
-	}
-
 	public static String getRequestPath(String servletPath, String queryString) {
 
 		String path = getPath(servletPath);
@@ -28,11 +16,6 @@ public class UrlUtil {
 		} else {
 			return path;
 		}
-	}
-
-	public static String getDirectory(String requestPath) {
-
-		return requestPath.split("/")[0];
 	}
 
 }

681eb22d3628cf649e0bab1b80a61e90cfac081c
chengjhan <chengjhan@gmail.com>
2018-10-05 19:03:04 +0800

    react

--- User/src/main/webapp/jsx/HelloWorld.jsx
+++ User/src/main/webapp/jsx/HelloWorld.jsx
@@ -1,11 +1,46 @@
 class HelloWorld extends React.Component {
     render() {
-        return <h1>{this.props.text}</h1>;
+        return <h1>Hello, {this.props.text}</h1>;
+    }
+
+    constructor() {
+        super();
+        this.state = {
+            data: [{
+                id: 1,
+                name: "Simon Bailey"
+            }, {
+                id: 2,
+                name: "Thomas Burleson"
+            }, {
+                id: 3,
+                name: "Will Button"
+            }]
+        }
+    }
+    render() {
+        const PersonRow = ( props ) => {
+            return (
+                <tr>
+                    <td>{props.data.id}</td>
+                    <td>{props.data.name}</td>
+                </tr>
+            );
+        }
+        let rows = this.state.data.map( person => {
+            return <PersonRow key={person.id} data={person} />
+        } );
+        return (
+            <table>
+                <tbody>{rows}</tbody>
+            </table>
+        );
     }
 }
+
 window.addEventListener( "load", () => {
     ReactDOM.render(
-        <HelloWorld text="Hello, world!" />,
+        <HelloWorld text="world!" />,
         document.getElementById( 'hello-world' )
     );
 } );
